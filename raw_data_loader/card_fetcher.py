4eb7dd493c5f24d8e505933d6b4d603616770a79
tteige <tim.teige91@gmail.com>
2024-01-23 11:03:22 +0100

    Add logging

--- raw_data_loader/card_fetcher.py
+++ raw_data_loader/card_fetcher.py
@@ -9,26 +9,30 @@ import requests
 def fetch_card_data(event=None, context=None):
     base_url = "https://api.scryfall.com"
 
+    print("Retrieving file metadata")
     resp = requests.get(f"{base_url}/bulk-data/default_cards")
     if resp.status_code != 200:
         print(f"Unable to retrieve bulk metadata. Error code: {resp.status_code} - {resp.reason}")
         raise ValueError
     download_uri = resp.json()["download_uri"]
 
+    print("Initializing download")
     resp = requests.get(download_uri)
     if resp.status_code != 200:
         print(f"Unable to retrieve bulk data. Error code: {resp.status_code} - {resp.reason}")
         raise ValueError
 
+    print("File downloaded")
     fp = f"raw-data/{download_uri.split('/')[-1]}"
 
     local = os.getenv("local")
 
     if local is None and not local:
-        print("File downloaded, writing to s3")
+        print("Writing to s3")
         s3_client = client("s3")
         resp = s3_client.put_object(Body=resp.json(), Bucket="mtg-pricing-data", Key=fp)
     else:
+        print("Writing to disk")
         with open(fp, "w") as f:
             json.dump(resp.json(), f)
 
